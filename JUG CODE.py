42d9d09b1b6df83c636320d1bd3961aca9c90612
Arjun Basava <basavapardhu@gmail.com>
2026-07-19 10:39:56 +0530

    Add files via upload

---
+++ JUG CODE.py
@@ -0,0 +1,12 @@
+def water_jug():
+    print("Water Jug Problem")
+    print("-----------------")
+
+    print("(0,0) -> Start")
+    print("(0,3) -> Fill Jug2")
+    print("(3,0) -> Pour Jug2 into Jug1")
+    print("(3,3) -> Fill Jug2")
+    print("(4,2) -> Pour Jug2 into Jug1")
+    print("Goal Achieved: 2 Litres in Jug2")
+
+water_jug()
